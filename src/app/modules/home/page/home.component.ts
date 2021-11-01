f37ef370ab79d4b76551c25e36fc3e3ba197ad4e
Kovalskiii <49062638+Kovalskiii@users.noreply.github.com>
2021-11-01 23:29:57 +0200

    added game buttons

--- src/app/modules/home/page/home.component.ts
+++ src/app/modules/home/page/home.component.ts
@@ -5,24 +5,28 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements AfterViewInit {
 
   /** Template reference to the canvas element */
-  @ViewChild('canvasEl', {static: false}) canvasEl!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvasEl', {static: false}) canvasEl!: ElementRef;
 
   /** Canvas 2d context */
   private context!: CanvasRenderingContext2D;
 
-  colsNum!: number;
-  rowsNum!: number;
-  resolution!: number;
+  colsNum!:          number;
+  rowsNum!:          number;
+  resolution!:       number;
   currentGridState!: number[][];
+  animReqId!:        number;
+  gameStarted:       boolean = false;
+  gameStopped:       boolean = false;
+  gridRandomized:    boolean = false;
 
   constructor() {}
 
   /** Build grid */
   getInitialGrid(): number[][]  {
-    this.canvasEl.nativeElement.width = 1600;
+    this.canvasEl.nativeElement.width = 1400;
     this.canvasEl.nativeElement.height = 700;
     this.resolution = 10;
     this.colsNum = this.canvasEl.nativeElement.width / this.resolution;
@@ -38,8 +42,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   /** Get next cells generation array */
- getNextGenCells(): number[][] {
-    //const currentCellsGen = this.currentGridState.map(arr => arr.map(cell => cell.this.currentGridState));
+  getNextGenCells(): number[][] {
     const currentCellsGen = this.currentGridState.map(arr => arr.slice());
 
     for (let col = 0; col < this.currentGridState.length; col++) {
@@ -76,39 +79,86 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   /** Draw(paint) cells on the grid ( 1 = live cell, 0 = dead cell) */
-  paintGridCells() {
+  paintGridCells(grid: number[][]) {
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d')!;
 
-    for (let col = 0; col < this.currentGridState.length; col++) {
-      for (let row = 0; row < this.currentGridState[col].length; row++) {
-        const cell = this.currentGridState[col][row];
+    for (let col = 0; col < grid.length; col++) {
+      for (let row = 0; row < grid[col].length; row++) {
+        const cell = grid[col][row];
 
         this.context.beginPath();
         this.context.rect(col * this.resolution, row * this.resolution, this.resolution, this.resolution);
         this.context.fillStyle = cell ? 'gray' : 'white';
         this.context.fill();
+        this.context.strokeStyle = 'lightgray';
         this.context.stroke();
+
       }
     }
   }
 
   /** Updates grid with new data */
   updateGrid() {
-    this.currentGridState = this.getNextGenCells();
-    this.paintGridCells();
-    requestAnimationFrame(this.updateGrid.bind(this));
+    if (!this.gameStopped) {
+      this.currentGridState = this.getNextGenCells();
+      this.paintGridCells(this.currentGridState);
+      this.animReqId = requestAnimationFrame(this.updateGrid.bind(this));
+    }
+  }
+
+  /** Starts game (cells animation) */
+  startGame() {
+    if (this.gridRandomized)
+    {
+      this.animReqId = requestAnimationFrame(this.updateGrid.bind(this));
+      this.gameStarted = true;
+      this.gridRandomized = false;
+    }
+    else if (!this.gameStarted && !this.gameStopped) {
+      const initialGrid = this.getInitialGrid();
+      this.currentGridState = this.randomizeInitialGrid(initialGrid);
+      this.animReqId = requestAnimationFrame(this.updateGrid.bind(this));
+      this.gameStarted = true;
+      this.gameStopped = false;
+    }
+    else {
+      this.animReqId = requestAnimationFrame(this.updateGrid.bind(this));
+      this.gameStarted = true;
+      this.gameStopped = false;
+    }
+
+  }
+
+  /** Stops game (cells animation) */
+  stopGame() {
+    if (this.animReqId) {
+      cancelAnimationFrame(this.animReqId);
+      this.gameStarted = false;
+      this.gameStopped = true;
+    }
+  }
+
+  /** Clear grid cells */
+  clearGrid() {
+    this.stopGame();
+    this.currentGridState = this.getInitialGrid();
+    this.paintGridCells(this.currentGridState);
+    this.gameStopped = false;
+    this.gridRandomized = false;
+  }
+
+  /** Fill grid with random values */
+  randomizeGrid() {
+    const initialGrid = this.getInitialGrid();
+    this.currentGridState = this.randomizeInitialGrid(initialGrid);
+    this.paintGridCells(this.currentGridState);
+    this.gridRandomized = true;
   }
 
   ngAfterViewInit() {
     const initialGrid = this.getInitialGrid();
-    this.currentGridState = this.randomizeInitialGrid(initialGrid);
-    requestAnimationFrame(this.updateGrid.bind(this));
+    this.paintGridCells(initialGrid);
   }
-
-  ngOnInit() {
-
-  }
-
 }
 
 
